8538dad7764b1237ef637f1fcc63a16fc2fe99eb
Zackary Cammack <zack.cammack@gmail.com>
2020-08-22 16:32:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,7 +24,7 @@ def monopoly_with_third_tier
 end
 
 def monopoly_with_fourth_tier
-  monopoly = {:railroads => 
+  monopoly = {:railroads =>
                 {:pieces=>4,
                  :names=>{:reading_railroad=>{"mortgage_value" => "$100"},
                           :pennsylvania_railroad=>{"mortgage_value" => "$200"},
